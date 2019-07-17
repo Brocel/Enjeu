b4d23b0035d2365471c16e9f1a6d49c5f4db4244
Brocel <geenens.prof@gmail.com>
2019-07-17 08:41:22 +0200

    Normalisation de global.js : développement des fonctions de manipulation du DOM -> un seul fichier js pour gérer sur tous les composants

--- composants/global/global.js
+++ composants/global/global.js
@@ -1,6 +1,10 @@
 //Manipulations du DOM
-//création d'éléments
-function creaElt(nomElt,plateau,classAff) {
+//création de composants
+  //plateau : id de la div où placer le composant
+  //classe : nom de la classe de la grille associée (pour le placement dans le document)
+  //nomComp : id du composant à créer
+  //chemin : chemin vers le composant à créer
+function creaComp(plateau,classe,nomComp,chemin) {
   //Variables
   var elt;
   var plat;
@@ -8,38 +12,40 @@ function creaElt(nomElt,plateau,classAff) {
   //Créer l'élément, lui ajouter les attributs en fction de la valeur de para (attention au nommage des fichier html)
   elt = document.createElement('object');
   elt.setAttribute('type', 'text/html');
-  elt.setAttribute('id',nomElt);
-  elt.setAttribute('class', classAff);
-  elt.setAttribute('data', '../elements/'+nomElt+'/'+nomElt+'.html');
+  elt.setAttribute('id',nomComp);
+  elt.setAttribute('class', classe);
+  elt.setAttribute('data', chemin);
   plat.appendChild (elt);
 }
-//remplacement d'éléments
-function remplacElt(oldElt,newElt,plateau,classAff) {
+//remplacement de composants
+  //oldComp : id composant à retirer
+  //newComp : id composant à créer
+function remplaceComp(plateau,classe,oldComp,newComp,chemin) {
   //Variables
-  var ancElt;
-  var nouvElt;
+  var ancComp;
+  var nouvComp;
   var plat;
-  ancElt = document.getElementById(oldElt);
+  ancComp = document.getElementById(oldComp);
   plat = document.getElementById(plateau);
-  nouvElt = document.createElement('object');
-  nouvElt.setAttribute('type', 'text/html');
-  nouvElt.setAttribute('id',newElt);
-  nouvElt.setAttribute('class', classAff);
-  nouvElt.setAttribute('data', '../elements/'+newElt+'/'+newElt+'.html');
-  plat.replaceChild(nouvElt,ancElt);
+  nouvComp = document.createElement('object');
+  nouvComp.setAttribute('type', 'text/html');
+  nouvComp.setAttribute('id',newComp);
+  nouvComp.setAttribute('class', classe);
+  nouvComp.setAttribute('data', chemin);
+  plat.replaceChild(nouvComp,ancComp);
 }
 //manipulation d'éléments
-function manipElt(oldElt,newElt,plateau,classAff) {
-  var ancElt = document.getElementById(oldElt);
-  if (ancElt==null) {
-    creaElt(newElt,plateau,classAff);
+function manipComp(plateau,classe,oldComp,newComp,chemin) {
+  var ancComp = document.getElementById(oldComp);
+  if (ancComp==null) {
+    creaComp(plateau,classe,nomComp,chemin);
   }else {
-    remplacElt(oldElt,newElt,plateau,classAff);
+    remplaceComp(plateau,classe,oldComp,newComp,chemin);
   }
 }
 //retirer des éléments
-function retirElt(elt,plateau) {
-  var eltEff = document.getElementById(elt);
+function retirComp(plateau,comp) {
+  var compEff = document.getElementById(comp);
   var plat = document.getElementById(plateau);
-  plat.removeChild(eltEff);
+  plat.removeChild(compEff);
 }
