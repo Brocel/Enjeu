df85d241329c00f0358b2f141aab40f6fb436caa
Brocel <geenens.prof@gmail.com>
2019-07-18 16:09:51 +0200

    validation du prompt test1 (enjeu.js)

--- composants/enjeu/enjeu.js
+++ composants/enjeu/enjeu.js
@@ -52,12 +52,13 @@ function retirComp(plateau,comp) {
 //Prompt bouton
 //param message : message du prompt
 //param cle : cle de la valeur enregistrée dans localStorage
-function promptBouton(message,cle) {
+//param alerte : message de l'alerte dans la fonction promptBouton
+function promptBouton(message,cle,alerte) {
   var resultatPrompt = prompt(message);
   if (resultatPrompt==null) {
     resultatPrompt = document.location.reload(true);
-  } else if (resultatPrompt=='') {
-    alert('Veuillez entrer un '+ message);
+  } else if (resultatPrompt!==/[A-Z]{1}[a-zA-ZÀ-ÿ/ ]{1,19}/) {
+    alert(alerte);
     resultatPrompt = document.location.reload(true);
   } else {
     console.log(resultatPrompt);
@@ -72,7 +73,7 @@ function bouton(idBouton){
     manipComp('plateauEnjeu','aff','affJ','tableJ');
     retirComp('plateauEnjeu','creaJ');
   } else if (idBouton=='bt2') {
-    promptBouton('Nouveau joueur (nom)','nomNouvJ');
+    promptBouton('Nouveau joueur (nom)','nomNouvJ','Entrer un nom de joueur valide (1ère lettre en majuscule, max char. : 20)');
     manipComp('plateauEnjeu','aff','tableJ','affJ',);
     manipComp('plateauEnjeu','aff2','oldComp','creaJ');
     retirComp('plateauEnjeu','conJ');
